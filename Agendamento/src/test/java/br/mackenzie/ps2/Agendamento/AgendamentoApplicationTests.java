5861d4ffb4979779e2e1c43403bb73f1d01e25ad
GabrielPalacios19 <gabriel.puccetti@gmail.com>
2025-03-25 20:13:34 -0300

    Add files via upload

---
+++ Agendamento/src/test/java/br/mackenzie/ps2/Agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ps2.Agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
